966c5fe034c6accdd40cc9a15b0b729b03cf0ad0
EnginCoban <141469133+EnginCoban@users.noreply.github.com>
2025-01-25 22:07:55 +0100

    platzhaltertext für email hinzugefuegt

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="container-fluid mt-4 text-break  ">
     <div class="row ">
         <footer class="text-center mb-0 ">
-            made by <a href="mailto:enginstgt@hotmail.de">Engin Coban</a>
+            made by <a href="mailto:empfaenger@example.com">Engin Coban</a>
         </footer>
 
     </div>
