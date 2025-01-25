d346ee6dd3e6cbe793f9ce5bc978f63f11d22f12
EnginCoban <141469133+EnginCoban@users.noreply.github.com>
2025-01-25 20:09:31 +0100

    send-mail.php bearbeitet

--- send-mail.php
+++ send-mail.php
@@ -121,7 +121,7 @@ if (isset($_POST['submit'])) {
 
             // Empfänger
             $mail->setFrom($safeEmail, $safeName);
-            $mail->addAddress(include 'username.php', 'Engin');
+            $mail->addAddress(include 'username.php', 'Name');
             
             // Inhalt der E-Mail
             $mail->isHTML(true);
