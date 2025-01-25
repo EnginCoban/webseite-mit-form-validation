a3a6df484afa0339209b9fdf228964a2c6d20a57
EnginCoban <141469133+EnginCoban@users.noreply.github.com>
2025-01-25 22:04:02 +0100

    Code sauber gemacht und styles angepasst

--- about-us-bootstrap.php
+++ about-us-bootstrap.php
@@ -7,7 +7,7 @@
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="bootstrap_uebungen.css" >
+  <link rel="stylesheet" href="bootstrap_uebungen.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link
     href="https://fonts.googleapis.com/css2?family=Agbalumo&family=Balsamiq+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Lobster&family=Mouse+Memoirs&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Pacifico&family=Quicksand:wght@300..700&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&family=Tangerine:wght@400;700&display=swap"
@@ -16,59 +16,58 @@
 
 <body>
   <?php include 'navbar.php'; ?>
+
   <div class="container-fluid banner-about-us">
     <div class="row">
       <h1 class="text-center speisen display-1">Über uns</h1>
     </div>
   </div>
+
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-4  " style="max-width: 500px;">
-    
-        <img  class="img-fluid rounded mx-auto d-block ki-bild  " width="500" src="images/_b99c8ec3-0485-4946-b7ea-24221914aad0.jpg" alt="">
-     
-      </div>
-    
-        <div class="col-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 fs-5  mt-4">
-          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-            ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-            rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
-            sit
-            amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no
-            sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-        </div>
-    
 
+      <div class="col-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mt-4  " style="max-width: 500px;">
+        <img class="img-fluid rounded mx-auto d-block ki-bild  " width="500" src="images/_b99c8ec3-0485-4946-b7ea-24221914aad0.jpg" alt="">
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 fs-5  mt-4">
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+          ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
+          sit
+          amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+          erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+          no
+          sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+      </div>
     </div>
+
     <div class="row justify-content-center">
       <div class="col-12 col-md-6   col-lg-6 col-xl-6 col-xxl-6 mt-4 fs-5 order-2 order-md-1  order-xl-1 ">
-       <p style="color: red;"> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
-        sit
-        amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-        no
-        sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+          ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
+          sit
+          amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+          erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+          no
+          sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
       </div>
-    
-        <div class="col-12 col-md-6   col-lg-6 col-xl-6 col-xxl-6  mt-4 order-1 order-md-1  order-xl-2 "  style="max-width: 500px;">
-          <img class="img-fluid rounded mx-auto   d-block lazyload" width="500" src="images/_5811fe5c-2900-410e-ac5d-e3bdb8875db2.jpg" alt="">
-          
-        </div>
-    
+
+      <div class="col-12 col-md-6   col-lg-6 col-xl-6 col-xxl-6  mt-4 order-1 order-md-1  order-xl-2 " style="max-width: 500px;">
+        <img class="img-fluid rounded mx-auto   d-block lazyload" width="500" src="images/_5811fe5c-2900-410e-ac5d-e3bdb8875db2.jpg" alt="">
+      </div>
 
     </div>
   </div>
+
   <?php include 'footer.php' ?>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js" async></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js" async></script>
 </body>
 
 </html>
